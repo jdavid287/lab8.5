ed7da2e8f57ddd8568b3e3c57a47a98a7d080521
Jack Davidson <133827574+jdavid287@users.noreply.github.com>
2024-03-18 20:17:30 +0000

    added table and js from supabase

--- .eleventy.js
+++ .eleventy.js
@@ -4,11 +4,15 @@ const supabaseUrl = 'https://lvrsghxuslrgfnzvrceo.supabase.co'
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imx2cnNnaHh1c2xyZ2ZuenZyY2VvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTA3ODk4MzYsImV4cCI6MjAyNjM2NTgzNn0.LRS0Pkr5eWfsL-EShTqPMdcQXDARjCsAuvX6u54tVR4'
 const supabase = createClient(supabaseUrl, supabaseKey)
 
-let { data: Books, error } = await supabase
+async function getBooks() {
+  let { data: Books, error } = await supabase
   .from('Books')
   .select('*')
 
 for (let book of books) {
   let bookList = document.getElementById('books');
-  bookList.innerHTML += `<tr><td>${book.title}</td><td>${book.publisher}</td></tr>`;
+  bookList.innerHTML += `<li>${book.title} - ${book.author}</li>`;
+  }
 }
+
+getBooks();
