a7c694dea42f442e2da9d41610d56a1060f78840
Jack Davidson <133827574+jdavid287@users.noreply.github.com>
2024-03-18 20:24:12 +0000

    added new js file and sync html to js

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import { createClient } from '@supabase/supabase-js'
 const supabaseUrl = 'https://lvrsghxuslrgfnzvrceo.supabase.co'
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imx2cnNnaHh1c2xyZ2ZuenZyY2VvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTA3ODk4MzYsImV4cCI6MjAyNjM2NTgzNn0.LRS0Pkr5eWfsL-EShTqPMdcQXDARjCsAuvX6u54tVR4'
 const supabase = createClient(supabaseUrl, supabaseKey)
+Script
 
 async function getBooks() {
   let { data: Books, error } = await supabase
